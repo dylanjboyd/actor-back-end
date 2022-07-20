6db8714b0651b3382dcdde1b3e549c258b1ca1f4
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-07-21 00:28:48 +1000

    feat: add Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Actor Back End')
+    .setDescription('Gives not-so-real information about actors and movies')
+    .setVersion('1.0')
+    .addTag('actors')
+    .addTag('movies')
+    .addTag('film')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
