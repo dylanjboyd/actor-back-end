85e3b06571af8dd6507efe6288a05c4d8dcd8406
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-07-21 08:27:19 +1000

    feat: add Swagger DTO type definitions

--- src/actors/dto/create-actor.dto.ts
+++ src/actors/dto/create-actor.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateActorDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   gender: number;
+
+  @ApiProperty()
   popularity: number;
+
+  @ApiProperty({ required: false })
   placeOfBirth?: string;
 }
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateMovieDto {
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   adult: boolean;
+
+  @ApiProperty()
   budget: number;
+
+  @ApiProperty({ required: false })
   tagline?: string;
 }
