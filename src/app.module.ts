dcd505990f86cfdb1244f855aa73ae510d8d2c7b
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-07-20 16:35:04 +1000

    feat: generate actor and movie CRUD resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ActorsModule } from './actors/actors.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [ActorsModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
