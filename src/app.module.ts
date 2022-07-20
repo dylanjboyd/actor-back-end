446c8a347374e40947eecf43b4becc34273164a9
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-07-21 01:37:29 +1000

    feat: auto-load entities to improve encapsulation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { ActorsModule } from './actors/actors.module';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Actor } from './actors/entities/actor.entity';
-import { Movie } from './movies/entities/movie.entity';
 
 const typeOrmImport = TypeOrmModule.forRoot({
   type: 'better-sqlite3',
   database: ':memory:',
   dropSchema: true,
-  entities: [Actor, Movie],
   synchronize: true,
+  autoLoadEntities: true,
 });
 
 @Module({
